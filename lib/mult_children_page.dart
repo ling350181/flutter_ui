50ecd4894b4b3644b3fca18c7ffed634b394de43
xunjin.chen <>
2023-01-28 13:38:38 +0900

    verticalDirection

--- lib/mult_children_page.dart
+++ lib/mult_children_page.dart
@@ -7,6 +7,7 @@ import 'package:flutter_ui/mult_child/wrap_cross_axis_alignment_page.dart';
 import 'package:flutter_ui/mult_child/wrap_direction_page.dart';
 import 'package:flutter_ui/mult_child/wrap_run_alignment_page.dart';
 import 'package:flutter_ui/mult_child/wrap_text_direction_page.dart';
+import 'package:flutter_ui/mult_child/wrap_vertical_direction_page.dart';
 
 class MultChildrenPage extends StatelessWidget {
   const MultChildrenPage({Key? key}) : super(key: key);
@@ -184,6 +185,27 @@ class MultChildrenPage extends StatelessWidget {
                   );
                 },
                 child: const Text("Wrap textDirection"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  minimumSize: Size(MediaQuery.of(context).size.width - 50, 50),
+                  side: const BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const WrapVerticalDirectionPage(),
+                    ),
+                  );
+                },
+                child: const Text("Wrap verticalDirection"),
               ),
             ),
           ],
