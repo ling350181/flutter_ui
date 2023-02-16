bbe9cc37f43aada6e498dda0f68666a7c8eceaba
xunjin.chen <>
2023-02-16 14:30:02 +0900

    curve

--- lib/animation/curve_page.dart
+++ lib/animation/curve_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui/animation/curve_demo_page.dart';
+import 'package:flutter_ui/animation/parabola_page.dart';
 
 class CurvePage extends StatelessWidget {
   const CurvePage({Key? key}) : super(key: key);
@@ -31,6 +32,27 @@ class CurvePage extends StatelessWidget {
                 );
               },
               child: const Text("Curve Demo"),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  minimumSize: Size(MediaQuery.of(context).size.width - 50, 50),
+                  side: const BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ParabolaPage(),
+                    ),
+                  );
+                },
+                child: const Text("放物線"),
+              ),
             ),
           ],
         ),
